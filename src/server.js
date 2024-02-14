3d4792f54b47e445799770f7f174c080979e74ec
anay5500 <139808859+anay5500@users.noreply.github.com>
2024-02-14 22:01:02 +0530

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const feedroutes = require('./routes/feed-route');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(bodyparser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Meathods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    next();
+});
+app.use('/feed', feedroutes);
+
+
+mongoose.connect('mongodb+srv://anaypapnoi24:999999999@cluster0.n5ohx7d.mongodb.net/?retryWrites=true&w=majority')
+    .then(result => { app.listen(5000); }).catch(err => { console.log(err); })
